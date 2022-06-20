f2b31f4a66f20f3d6b88d58c0f19732f2b1973f9
Ágata Marinho <106996641+agatamarinho@users.noreply.github.com>
2022-06-20 19:57:28 +0000

    .

--- Lista2/2ex3.cs
+++ Lista2/2ex3.cs
@@ -4,9 +4,13 @@ public class Program {
 
   public static void Main(string[] args) 
   {
-    int v1 = int.Parse(Console.ReadLine());
-    int v2 = int.Parse(Console.ReadLine());
-    int media = v1 + v2 / 6 ;
-    Console.WriteLine(media); 
+     Console.WriteLine("Nota do primeiro bimestre");
+     int n1 = int.Parse(Console.ReadLine());
+     Console.WriteLine("Nota do segundo bimestre");
+     int n2 = int.Parse(Console.ReadLine());
+     int p1 = 2;
+     int p2 = 3;
+     int media = (p1 * n1 + p2 * n2) / (p1+p2);
+     Console.WriteLine($"Média parcial {media}");
   }
 }
